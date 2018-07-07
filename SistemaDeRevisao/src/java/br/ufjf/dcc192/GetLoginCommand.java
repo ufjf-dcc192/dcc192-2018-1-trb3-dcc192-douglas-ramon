0d6ddaa00e2b87e0f22243af952d36131a2895ab
douglasBaumgratz <douglas.baumgratz@gmail.com>
2018-07-07 12:18:45 -0300

    index

--- SistemaDeRevisao/src/java/br/ufjf/dcc192/GetLoginCommand.java
+++ SistemaDeRevisao/src/java/br/ufjf/dcc192/GetLoginCommand.java
@@ -5,10 +5,24 @@
  */
 package br.ufjf.dcc192;
 
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 /**
  *
  * @author douglas
  */
-public class GetLoginCommand {
-    
+
+public class GetLoginCommand implements Comando {
+
+    @Override
+    public void exec(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispacher = request.getRequestDispatcher("/WEB-INF/login.jsp");
+        dispacher.forward(request, response);
+    }
+
 }
